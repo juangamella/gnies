a4c256e3a6c884d4ca1c640a106c80e4ec1f8ad4
gajuan <gajuan@student.ethz.ch>
2022-07-07 16:36:47 +0200

    Updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='gnies',
-    version='0.0.4',
+    version='0.0.1',
     author='Juan L. Gamella',
     author_email='juangamella@gmail.com',
     packages=['gnies', 'gnies.scores'],
