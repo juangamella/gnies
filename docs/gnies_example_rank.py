4c6526f4ecb2ddc60f5adea836366af317119c0b
Juan Gamella Martin <juan.gamella@stat.math.ethz.ch>
2022-07-18 14:13:11 +0200

    Refactoring of fit functions in gnies.main; module's API has changed; added new doc examples

---
+++ docs/gnies_example_rank.py
@@ -0,0 +1,20 @@
+import sempler
+import sempler.generators
+import gnies
+
+# Generate a random SCM using sempler
+W = sempler.generators.dag_avg_deg(10, 2.1, 0.5, 1)
+scm = sempler.LGANM(W, (0, 0), (1, 2))
+
+# Generate interventional data
+data = [
+    scm.sample(n=1000),
+    scm.sample(n=1000, noise_interventions={1: (0, 11)}),
+    scm.sample(n=1000, noise_interventions={2: (0, 12), 3: (0, 13)}),
+]
+
+# Run GnIES
+gnies.fit_rank(data, debug=1)
+
+# Run GnIES
+gnies.fit_rank(data, direction="backward", debug=1)
