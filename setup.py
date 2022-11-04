8bcaa95bec1111fe5371cc8b9990ba811ea74aa5
Juan Gamella Martin <juan.gamella@stat.math.ethz.ch>
2022-11-04 16:09:31 +0100

    Updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="gnies",
-    version="0.3.0",
+    version="0.3.1",
     author="Juan L. Gamella",
     author_email="juangamella@gmail.com",
     packages=["gnies", "gnies.scores"],
